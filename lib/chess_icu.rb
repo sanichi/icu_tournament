9f34ad25a15f052e4de1292c8cf44c6e0cd555dc
Mark Orr <mark.j.l.orr@googlemail.com>
2009-04-18 16:39:33 +0100

    Small change to get rid of warning messages when rake doc is run.

--- lib/chess_icu.rb
+++ lib/chess_icu.rb
@@ -1,3 +1,4 @@
+# :enddoc:
 dir = File.dirname(__FILE__)
 %w{util name player result tournament}.each { |file| require "#{dir}/#{file}" }
 %w{fcsv}.each                               { |file| require "#{dir}/tournament_#{file}" }
